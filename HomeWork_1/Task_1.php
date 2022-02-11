f0c8abe50f133c3a7bbacde56eb2f50b4dfd2c0b
Olena Novitska <Alenkanovitskaya7@gmail.com>
2022-02-11 15:10:43 +0200

    Task_1

--- HomeWork_1/Task_1.php
+++ HomeWork_1/Task_1.php
@@ -1,5 +1,27 @@
 <?php
 
-$a = 1;
+$a = 2;
+$b = 4;
+$c = 5;
 
-echo $a;
+if ($a > $b && $a > $c) {
+	echo "More number $a \n";
+} elseif ($b > $a && $b > $c) {
+	echo "More number $b \n";
+} elseif ($c > $a && $c > $b) {
+	echo "More number $c \n";
+}
+
+
+if ($a < $b && $a < $c) {
+	echo "Smaller number $a \n";
+} elseif ($b < $a && $b < $c) {
+	echo "Smaller number $b \n"; 
+} elseif ($c < $a && $c < $b) {
+	echo "Smaller number $c \n";
+}
+
+
+if ($a == $b && $b == $c) {
+	echo "$a \n";
+} 
