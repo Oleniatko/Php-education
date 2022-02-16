dcedba623cbe6c60358028e28add18771be75b09
Olena Novitska <Alenkanovitskaya7@gmail.com>
2022-02-16 19:04:25 +0200

    доделала дз после дедлайна...

--- HomeWork_2/Task_4.php
+++ HomeWork_2/Task_4.php
@@ -12,3 +12,44 @@ function echoDivisorsOfNumber($num) {
 }
 
 echoDivisorsOfNumber(20);
+
+
+
+
+
+//-------вирішення через рекурсію (зроблено в середу, в терміни виконання не вклалась)------
+//------ функція приймає два аргументи, чи підходить це під умови задачі? --------
+
+function echoDivisorsForNumber($num, $a) {
+	  
+	if ($num % $a == 0) {
+	 	echo $a . " дільник \n";
+	}
+	 
+	if ($a > 2) {
+	 	$a--;
+	 	echoDivisorsForNumber($num, $a);
+	}
+} 
+
+
+echoDivisorsForNumber(21, 20);
+
+//---- вирішення через рекурсію, варіант 2-----
+
+
+function echoDivisorsForNumber($num, $a) {
+	if ($a > 1 && $a < $num) {
+		if ($num % $a == 0) {
+	 	echo $a . " дільник \n";
+		}
+	}	  
+	
+	 if ($a < $num) {
+	 	$a++;
+	 	echoDivisorsForNumber($num, $a);
+	}
+} 
+
+
+echoDivisorsForNumber(21, 0);
